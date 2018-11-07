d041924900528d03aa22f8be427a3d29bdf73c09
Mark <Mark@SurfacePro>
2018-11-07 17:01:50 +0000

    Replace rating number with stars

--- src/components/HotelListElement.js
+++ src/components/HotelListElement.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Glyphicon } from 'react-bootstrap';
 
 export default class HotelListElement extends React.Component {
     render() {
         const hotel = this.props.hotel;
+        const maxRating = 5;
 
         return (
             <tr key={hotel.id}>
                 <td>{hotel.name}</td>
-                <td>{hotel.starRating}</td>
+                <td>
+                    {[...Array(maxRating)].map((item, key) =>
+                        <Glyphicon key={key} glyph={ "glyphicon " + (key < hotel.starRating ? "glyphicon-star" : "glyphicon-star-empty") } />
+                    )}
+                </td>
                 <td>{hotel.facilities.join(', ')}</td>
             </tr>
         );
